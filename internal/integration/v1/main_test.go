fb5e3df6f6340b468244b20447b0abf445fe9427
Lucas TESSON <lucastesson@protonmail.com>
2025-04-02 20:05:30 +0200

    fix: block integration tests if empty api key

--- internal/integration/v1/main_test.go
+++ internal/integration/v1/main_test.go
@@ -9,7 +9,7 @@ import (
 func TestMain(m *testing.M) {
 	// Get API key
 	apik, ok := os.LookupEnv("API_KEY")
-	if !ok {
+	if !ok || apik == "" {
 		fmt.Println("Can't run integration tests without an API key. Please request one at https://nvd.nist.gov/developers/request-an-api-key.")
 		os.Exit(1)
 	}
